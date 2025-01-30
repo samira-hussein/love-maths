b8c2091688a3a64cb38b94371dbaafaee5ece8f7
Samira Hussein <3odu9srq@students.codeinstitute.net>
2025-01-30 18:39:59 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,33 @@
-console.log("connected");
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
 
